86154c226f5832fe8d20b6a07133dd3276131132
vsco335533 <capitalvsco@gmail.com>
2025-12-23 14:19:11 +0530

    Change multer storage to memoryStorage and update limits

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-import path from "path";
+// import path from "path";
 
 // const storage = multer.diskStorage({
 //   destination: "uploads/",
@@ -8,7 +8,13 @@ import path from "path";
 //   }
 // });
 
+// export const upload = multer({
+//   storage,
+//   limits: { fileSize: 200 * 1024 * 1024 }, // 200MB
+// });
 export const upload = multer({
-  storage,
-  limits: { fileSize: 200 * 1024 * 1024 }, // 200MB
+  storage: multer.memoryStorage(), // ✅ Vercel-safe
+  limits: {
+    fileSize: 100 * 1024 * 1024, // 100MB
+  },
 });
