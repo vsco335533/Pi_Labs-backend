7227fc6bd5e21f98d99f2fdbb44f97af4b532578
vsco335533 <capitalvsco@gmail.com>
2025-12-23 14:06:05 +0530

    Update upload.js

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,12 +1,12 @@
 import multer from "multer";
 import path from "path";
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   }
+// });
 
 export const upload = multer({
   storage,
